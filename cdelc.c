a65adbb136fdebffe15c52cdce2acd9423825af8
Sam Dembicer <stdembicer@wm.edu>
2022-10-24 19:22:05 -0400

    Add classify_string

--- cdelc.c
+++ cdelc.c
@@ -1,7 +1,43 @@
-classify string
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#define MAXTOKENS 100
+#define MAXTOKENLEN 64
 
-<<<<<<< HEAD
+enum type_tag { IDENTIFIER, QUALIFIER, TYPE } ;
 
-=======
-gettoken
->>>>>>> feature/gettoken
+struct token {
+	char type;
+	char string[MAXTOKENLEN];
+};
+
+int top=-1;
+struct token stack[MAXTOKENS];
+struct token this;
+
+#define pop stack[top--]
+#define push(s) stack[++top]=s
+
+enum type_tag classify_string(void)
+{
+	char *s = this.string;
+	if (!strcmp(s,"const")) {
+		strcpy(s,"read-only");
+		return QUALIFIER;
+	}
+	if (!strcmp(s,"volatile")) return QUALIFIER;
+	if (!strcmp(s, "void")) return TYPE;
+	if (!strcmp(s, "char")) return TYPE;
+	if (!strcmp(s,"signed")) return TYPE;
+	if (!strcmp(s,"unsigned")) return TYPE;
+	if (!strcmp(s,"short")) return TYPE;
+	if (!strcmp(s,"int")) return TYPE;
+	if (!strcmp(s,"long")) return TYPE;
+	if (!strcmp(s,"float")) return TYPE;
+	if (!strcmp(s,"double")) return TYPE;
+	if (!strcmp(s,"struct")) return TYPE;
+	if (!strcmp(s,"union")) return TYPE;
+	if (!strcmp(s,"enum")) return TYPE;
+	return IDENTIFIER;
+}
