0d909b2981db10dc6dd8ee4446101cd5c07a8fc9
Sam Dembicer <stdembicer@wm.edu>
2022-11-08 16:48:44 -0500

    Add stack_hac_2.c

---
+++ ASSIGNMENT_08/Part_II/stack_hack_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>;
+
+int initialized_data = 5;
+
+main() {
+	int *heap;
+	printf("main's text segment is located at %p\n", &main);
+	printf("while initialized_data's initialized data segment is located at %p\n", &initialized_data);
+	printf("and the heap is located at %p\n", &heap);
+	return 0;
+}
