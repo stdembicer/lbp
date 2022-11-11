209b17a9c6426b3047f87bbae1b1f9af6adc4b0b
Sam Dembicer <stdembicer@wm.edu>
2022-11-11 13:41:24 -0500

    Update AS08.pdf

--- ASSIGNMENT_08/Part_II/stack_hack_3.c
+++ ASSIGNMENT_08/Part_II/stack_hack_3.c
@@ -4,8 +4,9 @@ int initialized_data = 5;
 
 main() {
         int *heap;
-        // printf("main's text segment is located at %p\n", &main);
-        // printf("while initialized_data's initialized data segment is located at %p\n", &initialized_data);
-       //  printf("and the heap is located at %p\n", &heap);
-        return 0;
+ //       printf("main's text segment is located at %p\n", &main);
+   //     printf("while initialized_data's initialized data segment is located at %p\n", &initialized_data);
+     //   printf("and the heap is located at %p\n", &heap);
+	printf("I grew the stack by initializng a global function and calling it.The stack grew to %p\n", &initialized_data);
+       	return 0;
 }
